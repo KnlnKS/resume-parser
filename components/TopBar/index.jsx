6941a7650df5ada4eea01bd7ea3bd0ec98c2a253
KnlnKS <soulkks@hotmail.ca>
2021-08-09 01:28:34 -0400

    ✏️ Fix Typo

--- components/TopBar/index.jsx
+++ components/TopBar/index.jsx
@@ -15,10 +15,10 @@ const TopBar = () => {
           How well does your resume get parsed?
         </Text>
         <Text fontSize="md">
-          This tool uses Lever's resume parsing API to parse resumes. Use this
+          {`This tool uses Lever's resume parsing API to parse resumes. Use this
           to see how well your resume is read by Application Tracking Systems
           (ATS) when applying to jobs. Companies that use Lever for job apps
-          include: Figma, Palantir, Netflix, Twitch, Yelp and several others.
+          include: Figma, Palantir, Netflix, Twitch, Yelp and several others.`}
         </Text>
       </Box>
       <Box mb="4" mt="4" pl="20" pr="20">
@@ -29,7 +29,7 @@ const TopBar = () => {
           p={0.5}
           isExternal
         >
-          Jafer's Lever Parser <ExternalLinkIcon mx="2px" />
+          {`Jafer's Lever Parser`} <ExternalLinkIcon mx="2px" />
         </Link>{' '}
         and{' '}
         <Link
@@ -38,7 +38,7 @@ const TopBar = () => {
           p={0.5}
           isExternal
         >
-          KnlnKS' Lever Extension <ExternalLinkIcon mx="2px" />
+          {`KnlnKS' Lever Extension`} <ExternalLinkIcon mx="2px" />
         </Link>
         .
       </Box>
