34af58c51737df7b2fa2048962e7636ed1d81a6b
KnlnKS <soulkks@hotmail.ca>
2021-08-09 17:04:20 -0400

    ✏️ Fix Typo

--- hooks/useCustomToast.jsx
+++ hooks/useCustomToast.jsx
@@ -1,29 +1,31 @@
-import { useToast } from '@chakra-ui/react';
-import { MAX_FILE_SIZE, TOAST_DURATION } from "../constants"
+import { useToast } from "@chakra-ui/react";
+import { MAX_FILE_SIZE, TOAST_DURATION } from "../constants";
 
 const useCustomToast = () => {
   const toast = useToast();
 
   const successToast = () =>
     toast({
-      title: 'Resume successfully parsed!',
-      status: 'success',
+      title: "Resume successfully parsed!",
+      status: "success",
       duration: TOAST_DURATION,
       isClosable: true,
     });
-  const errorToast = err =>
+  const errorToast = (err) =>
     toast({
-      title: 'Resume parsing failed!',
+      title: "Resume parsing failed!",
       description: JSON.stringify(err.message),
-      status: 'error',
+      status: "error",
       duration: TOAST_DURATION,
       isClosable: true,
     });
   const fileSizeErrorToast = () =>
     toast({
-      title: 'Resume upload error!',
-      description: `File too big. Maximum size of ${MAX_FILE_SIZE}MB.`,
-      status: 'error',
+      title: "Resume upload error!",
+      description: `File too big. Maximum size of ${
+        MAX_FILE_SIZE / 1000000
+      }MB.`,
+      status: "error",
       duration: TOAST_DURATION,
       isClosable: true,
     });
