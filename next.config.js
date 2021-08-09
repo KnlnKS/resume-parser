f658397f0c5a2f5a9de15a71d16e239a588e51eb
KnlnKS <soulkks@hotmail.ca>
2021-08-09 02:47:47 -0400

    ⚡️ Add Service Worker

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
+
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? withPWA({
+        pwa: {
+          dest: "public",
+        },
+      })
+    : {};
