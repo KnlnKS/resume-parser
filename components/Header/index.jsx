c642945d2be453d3805a5b85d3772a859432f2f3
KnlnKS <soulkks@hotmail.ca>
2021-08-09 02:24:35 -0400

    🔍️ Add Header

---
+++ components/Header/index.jsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import React from "react";
+
+const Header = () => (
+  <Head>
+    <meta charSet="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+    />
+    <title>Resume Parser</title>
+    <meta name="title" content="Resume Parser" />
+    <meta
+      name="description"
+      content="Tool to see how well top companies such as Figma, Palantir, Netflix, Twitch, Yelp view your resume."
+    />
+    <meta
+      name="keywords"
+      content="resume,resume parser,lever,lever resume parser,lever parser,resume reader,lever api, lever resume api, lever resume parser api"
+    />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://resume-parser.vercel.app/" />
+    <meta property="og:title" content="Resume Parser" />
+    <meta
+      property="og:description"
+      content="Tool to see how well top companies such as Figma, Palantir, Netflix, Twitch, Yelp view your resume."
+    />
+    <meta
+      property="og:image"
+      content="https://resume-parser.vercel.app/preview.png"
+    />
+    <meta
+      property="twitter:card"
+      content="https://resume-parser.vercel.app/preview.png"
+    />
+    <meta property="twitter:url" content="https://resume-parser.vercel.app/" />
+    <meta property="twitter:title" content="Resume Parser" />
+    <meta
+      property="twitter:description"
+      content="Tool to see how well top companies such as Figma, Palantir, Netflix, Twitch, Yelp view your resume."
+    />
+    <meta
+      property="twitter:image"
+      content="https://resume-parser.vercel.app/preview.png"
+    />
+  </Head>
+);
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import DataDisplay from "../components/DataDisplay";
+import Header from "../components/Header";
 import TopBar from "../components/TopBar";
 import { useAnalytics } from "../hooks";
 
 export default function Home() {
-  useAnalytics("page_view")
+  useAnalytics("page_view");
   return (
     <>
+      <Header />
       <TopBar />
       <DataDisplay />
     </>
