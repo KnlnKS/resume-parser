01910c7bbd95cabf003f5f6785000fc273f529d6
KnlnKS <soulkks@hotmail.ca>
2021-08-09 03:02:02 -0400

    🔥 Remove Color Mode Script

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,18 @@ import "../styles/dataDisplay.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/inter/700.css";
 
-import { ColorModeScript, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
     heading: "Inter",
     body: "Open Sans",
   },
-  initialColorMode: "light",
-  useSystemColorMode: false,
 });
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <ColorModeScript />
       <Component {...pageProps} />
     </ChakraProvider>
   );
