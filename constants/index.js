aae6c265b5ff863cf8210e5d943974fc13efb8fd
KnlnKS <soulkks@hotmail.ca>
2021-08-09 20:37:40 -0400

    🐍 Bypass Analytics Block

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 
-export const API_URL = "/api/parse";
+export const API_URL = "/api/events";
 export const ANALYTICS_URL = "/api/analytics";
 export const MAX_FILE_SIZE = 3 * 1000 * 1000; // 3 MB
 export const TOAST_DURATION = 3000;
