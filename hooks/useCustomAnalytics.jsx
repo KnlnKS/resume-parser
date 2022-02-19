d76c4526b4e947a56f619ae61d0787b7393aeb90
KnlnKS <soulkks@hotmail.ca>
2022-02-19 15:48:22 -0500

    ⚡️ Remove Console Log

--- hooks/useCustomAnalytics.jsx
+++ hooks/useCustomAnalytics.jsx
@@ -4,11 +4,7 @@ import { ANALYTICS_URL } from "../constants";
 
 const useCustomAnalytics = (event) => {
   useEffect(() => {
-    fetch(ANALYTICS_URL, { method: "POST", body: event })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data?.message) console.log(data.message);
-      });
+    fetch(ANALYTICS_URL, { method: "POST", body: event });
   }, []);
 };
 
